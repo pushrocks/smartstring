fb4a03ad37216612cb66cd0f13b786c31b9ead88
Phil Kunz <github@philkunz.com>
2019-01-12 20:12:58 +0100

    fix(core): update

---
+++ test/test.normalize.ts
@@ -0,0 +1,19 @@
+import { tap, expect } from '@pushrocks/tapbundle';
+
+import * as smartstring from '../ts/index';
+
+tap.test('should normalize a string', async () => {
+  const testString = `
+    myawesome string;
+      is indented with two spaces
+  `
+  const normalizedString = smartstring.normalize.standard(testString);
+  expect(normalizedString).to.equal(
+`
+myawesome string;
+  is indented with two spaces
+  `
+  )
+})
+
+tap.start()
--- ts/smartstring.create.ts
+++ ts/smartstring.create.ts
@@ -1,13 +1,29 @@
 import * as plugins from './smartstring.plugins';
 
+/**
+ * creates a random string
+ * 
+ * ```ts
+ * createRandomString('AAAA')
+ * //=> 'AGHR' 
+ * ```
+ * 
+ * @param patternArg the pattern argument to use, Aa0!* are viable pattern descritors
+ * @param lengthArg  the length of the random string
+ * @param optionsArg options
+ */
 export const createRandomString = (
   patternArg: string,
-  lengthArg: number,
-  optionsArg: any
+  lengthArg?: number,
+  optionsArg?: any
 ): string => {
   return plugins.randomatic(patternArg, lengthArg, optionsArg);
 };
 
-export const createCryptoRandomString = (lengthArg): string => {
+/**
+ * creates a crytic string in the speicifed length
+ * @param lengthArg the length of the crypto string
+ */
+export const createCryptoRandomString = (lengthArg: number): string => {
   return plugins.cryptoRandomString(lengthArg);
 };
